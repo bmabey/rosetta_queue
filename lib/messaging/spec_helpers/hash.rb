e305cef111fa8dccd68697ae217d85a1f13ab142
Ben Mabey <ben@benmabey.com>
2008-10-09 13:36:52 -0600

    added timeout and better messaging for story matcher

--- lib/messaging/spec_helpers/hash.rb
+++ lib/messaging/spec_helpers/hash.rb
@@ -7,8 +7,15 @@ class Hash
   # expected_message.should be_published_to(:advertiser_create)
   #
   def published_to?(destination)
+    received_message = nil
+    begin
+      Timeout::timeout(2) { received_message = Messaging::Consumer.receive(destination).to_hash_from_json }
+    rescue Timeout::Error
+      raise "#{destination} should have received a message but did not NOTE: make sure there are no other processes which are polling messages"
+    end
+    
     # calling should == is kinda wierd, I know.. but in order to get a decent error message it is needed
-    Messaging::Consumer.receive(destination).to_hash_from_json.should ==  self
+    received_message.should ==  self
   end
   
 end
