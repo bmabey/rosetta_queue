ef9ef6d341830872fef055909346cf7792ea5c79
Ben Mabey <ben@benmabey.com>
2009-01-29 22:13:53 -0700

    ack.. forgot to take out require when I removed consumer_manager.rb

--- lib/rosetta_queue.rb
+++ lib/rosetta_queue.rb
@@ -1,11 +1,9 @@
-require 'rubygems'
 require 'activesupport' # TODO: remove dependency
 
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__))) unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
 
 require 'rosetta_queue/adapter'
 require 'rosetta_queue/base'
-require 'rosetta_queue/consumer_manager'
 require 'rosetta_queue/consumer'
 require 'rosetta_queue/destinations'
 require 'rosetta_queue/exceptions'
@@ -14,7 +12,6 @@ require 'rosetta_queue/logger'
 require 'rosetta_queue/message_handler'
 require 'rosetta_queue/producer'
 
-                               
 if defined?(Rails)
   RosettaQueue.logger = RosettaQueue::Logger.new(File.join(Rails.root, 'log', 'rosetta_queue.log'))
   require('rosetta_queue/spec_helpers') if Rails.env == "test"
