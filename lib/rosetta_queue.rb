f68b6a4093d37c8cc2d9139f389c47e293ad24b4
Ben Mabey <ben@benmabey.com>
2009-07-27 20:52:49 -0600

    only require core_ext/string if ActiveSupport isn't present

--- lib/rosetta_queue.rb
+++ lib/rosetta_queue.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__))) unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
 
-require 'rosetta_queue/core_ext/string'
+require('rosetta_queue/core_ext/string') unless defined? ActiveSupport
 
 require 'rosetta_queue/adapter'
 require 'rosetta_queue/base'
